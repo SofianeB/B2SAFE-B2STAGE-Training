38726b711385ea362d239cb0f09aa6135f7baedd
chStaiger <christine.staiger@surfsara.nl>
2016-08-25 10:58:51 +0200

    DI4R hands-on material.

---
+++ ExampleTrainings/DI4R_examples/helloworld.r
@@ -0,0 +1,8 @@
+HelloWorld{
+    if(*name=="<YourName>"){
+        writeLine("stdout", "Hello *name!");
+        }
+    else { writeLine("stdout", "Hello world!"); }
+}
+INPUT *name="YourName"
+OUTPUT ruleExecOut, *name
